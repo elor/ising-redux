2c83c87149d0b0a28c654cd671e5491d6b02bf29
Erik E. Lorenz <erik@tuvero.de>
2023-03-03 11:43:34 +0100

    Catch2 requires far less bootstrapping; keeping it simple

--- test.cpp
+++ test.cpp
@@ -1,14 +1,9 @@
 #include "catch2/catch_test_macros.hpp"
-#define CATCH_CONFIG_MAIN
-#include "catch2/catch_all.hpp"
 
-#include<fmt/format.h>
+#include <fmt/format.h>
 
-TEST_CASE("test", "[test]") {
-    REQUIRE(1 == 1);
-}
+TEST_CASE("test", "[test]") { REQUIRE(1 == 1); }
 
 TEST_CASE("fmt_test", "[fmt]") {
-    REQUIRE(fmt::format("Hello, {}!", "World") == "Hello, World!");
+  REQUIRE(fmt::format("Hello, {}!", "World") == "Hello, World!");
 }
-
