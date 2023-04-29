d19ef0e6407e5295b39f78f0576f9cc24fb4ce0a
Erik E. Lorenz <erik@tuvero.de>
2023-04-29 19:16:20 +0200

    update install-build-and-test.sh

--- install-build-and-test.sh
+++ install-build-and-test.sh
@@ -6,10 +6,10 @@ set -e -u
 
 mkdir -p build
 
+cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake --build build
+(
 cd build
-
-conan install .. --build=missing
-cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-cmake --build .
 ctest -V
+)
 
